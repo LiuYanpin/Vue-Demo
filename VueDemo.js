36089be0188d903024af6460450a2e0db51788ea
yanpliu <yanpliu@thoughtworks.com>
2018-12-08 15:40:27 +0800

    custom component

--- VueDemo.js
+++ VueDemo.js
@@ -50,12 +50,18 @@ var app6 = new Vue({
 })
 
 Vue.component('todo-item', {
-  template: '<li>这是个代办项</li>'
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
 })
 
 var app7 = new Vue({
   el: '#app-7',
   data: {
+    groceryList: [
+      { id: 0, text: '蔬菜' },
+      { id: 1, text: '奶酪' },
+      { id: 2, text: '随便其他什么人吃的东西' }
 
+    ]
   }
 })
